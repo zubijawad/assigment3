745be71f6a9ea723604fd9f5370d84ce8727eca2
zubijawad <zubijawad495@gmail.com>
2025-06-22 06:08:34 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+// chapter 9
+// answer1
+var a = 10;
+document.write('result').innerHTML += 'the value of ++a is: ${++a}<br';
+document.write('result').innerHTML += 'now the value of is: ${a}<br>';
+var b = 11;
+document.write('result').innerHTML += 'the value of a++ is: ${b++}<br>';
+document.write('result').innerHTML += 'the value of ++ is: ${b}<br>';
+var c = 11;
+document.write('result').innerHTML += 'the value of --a is: ${--c}<br>';
+document.write('result').innerHTML += 'the value of a is: ${c}<br>';
+var d = 11;
+document.write('result').innerHTML += 'the value of a-- is: ${d--}<br>';
+document.write('result').innerHTML += 'the value of a is: ${d}<br>';
+// answer2
+var a = 2, b = 1;
+var =(-2) - (=1) + 2 + 2
+var = -2 + 1 + 2 + 2
+var = 3
+// answer 3
+name = input("please enter your name:")
+print("Hwllo," + name + "zulaikha hanif")
+// answer4
+var input = dovument.write('5').value;
+var num;
+if(input === "") {
+    num = 5;
+} else {
+    num = parseInt(input);
+}
+if (isNaN(num)) {
+    tableOutput.innerHTML ="please enter a valid number";
+}
+for (let i = 1; i <= 10; i++) {
+    tableHTML += {num} * {i} ={num * i};
+}
+// answer 6
+a) . var ("English", "Mathes","urdu")
+b) . var = (100, total_marks_per_subject)
+c) . var = ( e . g , obtained_marks_subject1);
+d) . int englishMarks, mathMarks ,urduMarks;
+print ("Enter marks for English:");
+Scanf("54%" , &englishMarks);
+printf("Enter marks for Math":);
+scanf("54%", &mathmarks);
+printf("Enter marks for urdu:");
+scanf("58%", &urduMarks);
+e) int totalMarks = englishMarks + ,athMarks + urduMarks;
+float percentage = (float)totalMarks / 300 * 100;
+
